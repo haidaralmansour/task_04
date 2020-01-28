a051f62bca1986b4b30792686d38c1d37bd623bd
Haidar AlMansour <haidar.almansour@zain.com>
2020-01-28 12:54:00 +0300

    love

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 120)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    def __str__(self):
+        return "%d, %s, time %s, %s" %(self.id, self.name, self.opening_time, self.closing_time)
+
